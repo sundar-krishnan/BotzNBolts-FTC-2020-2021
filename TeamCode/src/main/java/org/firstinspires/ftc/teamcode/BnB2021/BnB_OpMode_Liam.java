b784ee9fe7af950ad6ebbb6d2356439cdf638dc2
Liam Murphy <73758715+LiamM2005@users.noreply.github.com>
2020-12-20 15:47:29 -0500

    I added my file
    I added my file to team code and corrected Pranav's code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BnB2021/BnB_OpMode_Liam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BnB2021/BnB_OpMode_Liam.java
@@ -1,3 +1,32 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.firstinspires.ftc.teamcode.BnB2021;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -5,15 +34,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-package org.firstinspires.ftc.teamcode.BnB2021;
-
-        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-        import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.DcMotorSimple;
-        import com.qualcomm.robotcore.hardware.Servo;
-        import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 /**
@@ -29,9 +49,9 @@ package org.firstinspires.ftc.teamcode.BnB2021;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="BnB_OpMode_Pranav", group="Linear Opmode")
+@TeleOp(name="BnB_OpMode_Liam", group="Linear Opmode")
 //@Disabled
-public class BnB_OpMode_Pranav extends LinearOpMode {
+public class BnB_OpMode_Liam extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -39,15 +59,12 @@ public class BnB_OpMode_Pranav extends LinearOpMode {
     private DcMotor rightFrontDrive = null;
     private DcMotor leftBackDrive = null;
     private DcMotor rightBackDrive = null;
-    private DcMotor throwerDrive = null;
-    private DcMotor ringrollerDrive = null;
     private Servo grabberServo = null;
     private Servo collectorServo = null;
-    //    private Servo topClawServo = null;
+//    private Servo topClawServo = null;
 //    private Servo leftClawWristServo = null;
 //    private Servo rightClawWristServo = null;
     private DcMotor armLifter = null;
-
 
     private double servoPosition = 0.0;
     static final double INCREMENT = 0.1;     // amount to slew servo each CYCLE_MS cycle
@@ -73,7 +90,6 @@ public class BnB_OpMode_Pranav extends LinearOpMode {
         initializeDriveMotor();
         initializeServoMotor();
         initializeArmMotor();
-        initializeThrowerMotor();
 //        initializeServoMotorWrist();
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -228,21 +244,6 @@ public class BnB_OpMode_Pranav extends LinearOpMode {
         rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
-
-    private void initializeThrowerMotor()
-    {
-        throwerDrive = hardwareMap.get(DcMotor.class, "Thrower");
-        ringrollerDrive = hardwareMap.get(DcMotor.class, "RingRoller");
-
-        throwerDrive.setDirection(DcMotor.Direction.FORWARD);
-        ringrollerDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        throwerDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        ringrollerDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        throwerDrive.setPower(100.0);
-        ringrollerDrive.setPower(1.0);
-    }
-
     private void initializeServoMotor()
     {
         grabberServo = hardwareMap.get(Servo.class, "Grabber");
@@ -332,4 +333,3 @@ public class BnB_OpMode_Pranav extends LinearOpMode {
 //
 //    }
 }
-
